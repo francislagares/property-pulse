c5efb247b68feff96e478f427a93e429e6aa79b9
Francis Lagares <francis.lagares@gmail.com>
2024-03-10 14:54:58 +0100

    feat: create Hero section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from '@/components';
+
 const HomePage = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      WELCOME PAGE
+    <main>
+      <Hero />
     </main>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
+export * from './hero/Hero';
 export * from './navbar/NavBar';
